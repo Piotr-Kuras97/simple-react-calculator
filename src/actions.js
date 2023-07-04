ff1dff3c6e4b3bb3997ab9884b1a45141fe73b2c
Piotr Kuraś <piotr.kuras.1997@gmail.com>
2023-07-04 20:05:38 +0200

    first commit

---
+++ src/actions.js
@@ -0,0 +1,8 @@
+export const ACTIONS = {
+    ADD_DIGIT: 'add_digit',
+    CHOOSE_OPERATION: 'choose_operation',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete_digit',
+    EVALUATION: 'evaluation'
+}
+
